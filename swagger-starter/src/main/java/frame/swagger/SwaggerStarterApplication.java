cff7d440c3af49a33bcf847aafe3884baca3b01a
yangjiajun <yangjiajun999@126.com>
2018-07-03 08:57:59 +0800

    init

---
+++ swagger-starter/src/main/java/frame/swagger/SwaggerStarterApplication.java
@@ -0,0 +1,12 @@
+package frame.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerStarterApplication.class, args);
+    }
+}
